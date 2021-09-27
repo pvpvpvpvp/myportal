459b77d8cfb4a316dc87b8d50771dc7fc7dc08b9
남승균 <skyun.nam@gmail.com>
2021-09-27 15:28:43 +0900

    Initial Commit

---
+++ myportal/src/main/java/com/bitacademy/myportal/controllers/MainController.java
@@ -0,0 +1,15 @@
+package com.bitacademy.myportal.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MainController {
+	@RequestMapping({"/", "/main"})
+	public ModelAndView home() {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("/WEB-INF/views/home.jsp");
+		return mav;
+	}
+}
