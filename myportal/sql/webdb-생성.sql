2fdc8dc44cc1d8b1e1f498fd0816a6353a0b042c
남승균 <skyun.nam@gmail.com>
2021-09-30 09:00:35 +0900

    Add SQL

---
+++ myportal/sql/webdb-생성.sql
@@ -0,0 +1,8 @@
+-- 사용자 생성
+CREATE USER C##WEBDB IDENTIFIED BY webdb;
+
+-- COOECT, RESOURCE 권한 부여
+GRANT CONNECT, RESOURCE TO C##WEBDB;
+
+-- 테이블 스페이스 생성(12이상)
+ALTER USER C##WEBDB DEFAULT TABLESPACE users QUOTA unlimited ON users;
