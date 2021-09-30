1306163b1072f6f4e76435d5b0e9d79595ae37d7
남승균 <skyun.nam@gmail.com>
2021-09-30 13:46:57 +0900

    게시판 서비스

--- myportal/src/main/java/com/bitacademy/myportal/service/GuestbookServiceImpl.java
+++ myportal/src/main/java/com/bitacademy/myportal/service/GuestbookServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.bitacademy.myportal.repository.GuestbookDao;
 import com.bitacademy.myportal.repository.GuestbookVo;
@@ -18,17 +19,19 @@ public class GuestbookServiceImpl implements GuestbookService {
 		List<GuestbookVo> list = guestbookDaoImpl.selectAll();
 		return list;
 	}
-
+	
+	@Transactional
 	@Override
 	public boolean writeMessage(GuestbookVo vo) {
-		// TODO Auto-generated method stub
-		return false;
+		int insertedCount = guestbookDaoImpl.insert(vo);
+		System.out.println("삽입된 방명록 레코드:" + insertedCount);
+		return 1 == insertedCount;
 	}
 
 	@Override
 	public boolean deleteMessage(GuestbookVo vo) {
-		// TODO Auto-generated method stub
-		return false;
+		int deletedCount = guestbookDaoImpl.delete(vo);
+		return 1 == deletedCount;
 	}
 
 }
